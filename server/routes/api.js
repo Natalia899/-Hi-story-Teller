9870de05536a262a864b64e218f882e24c20120f
toam <toamcohen.transformation@gmail.com>
2021-01-19 22:21:55 +0200

    suggestions not complete yet

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,99 +1,102 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const Event = require("../models/event")
-const User = require("../models/user")
-const data = require('./data')
+const Event = require("../models/event");
+const User = require("../models/user");
+const data = require("./data");
 
-// data.forEach(event=> {
-//     let newEvent = new Event({
-//         title: event.title,
-//         startDate: event.startDate,
-//         endDate: event.endDate,
-//         countries: event.countries,
-//         gallery: event.gallery,
-//         description: event.description,
-//         approved: false
-//     })
-//     newEvent.save()
-// })
+// data.forEach((event) => {
+// 	let newEvent = new Event({
+// 		userName: event.userName,
+// 		title: event.title,
+// 		startDate: event.startDate,
+// 		endDate: event.endDate,
+// 		countries: event.countries,
+// 		gallery: event.gallery,
+// 		description: event.description,
+// 		approved: false,
+// 	});
+// 	newEvent.save();
+// });
 
 let user1 = new User({
-    username: "Ben",
-    password: "1234",
-    type: 'admin'
-})
+	username: "Ben",
+	password: "1234",
+	type: "admin",
+});
 
 let user2 = new User({
-    username: "Rose",
-    password: "9874",
-    type: 'guest'
-})
+	username: "Rose",
+	password: "9874",
+	type: "guest",
+});
 
-const usersDB = [user1, user2]
+const usersDB = [user1, user2];
 // usersDB.forEach(u=> u.save())
 
-router.get('/events/:start/:end/:country', async (req, res) => {
-    const { start, end, country } = req.params
-    let events = await Event.find({ countries: { $in: [`${country}`] }, startDate: { $gte: `${start}` }, endDate: { $lte: `${end}` } }).exec()
-    res.send(events)
-})
+router.get("/events/:start/:end/:country", async (req, res) => {
+	const { start, end, country } = req.params;
+	let events = await Event.find({
+		countries: { $in: [`${country}`] },
+		startDate: { $gte: `${start}` },
+		endDate: { $lte: `${end}` },
+	}).exec();
+	res.send(events);
+});
 
-router.post('/event', async (req, res) => {
-    const newEvent = new Event({ ...req.body, approved: false })
-    newEvent.save()
-    res.send(newEvent)
-})
+router.post("/event", async (req, res) => {
+	const newEvent = new Event({ ...req.body, approved: false });
+	newEvent.save();
+	res.send(newEvent);
+});
 
-router.post('/signUp', async (req, res) => {
-    const newUser = new User({ ...req.body, type: 'guest' })
-    newUser.save()
-    res.send(newUser)
-})
+router.post("/signUp", async (req, res) => {
+	const newUser = new User({ ...req.body, type: "guest" });
+	newUser.save();
+	res.send(newUser);
+});
 
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body
-    console.log(req.body)
-    let relUser = await User.findOne({ username, password})
-    res.send(relUser)
-})
+router.post("/login", async (req, res) => {
+	const { username, password } = req.body;
+	console.log(req.body);
+	let relUser = await User.findOne({ username, password });
+	res.send(relUser);
+});
 
-router.delete('/event/:id', async (req, res) => {
-    const { id } = req.params
-    let relEvent = await Event.findOneAndDelete({ _id: id })
-    res.send(relEvent)
-})
+router.delete("/event/:id", async (req, res) => {
+	const { id } = req.params;
+	let relEvent = await Event.findOneAndDelete({ _id: id });
+	res.send(relEvent);
+});
 
-router.put('/event/:id', async (req, res) => {
-    const { id } = req.params
-    await Event.findOneAndDelete({ _id: id })
-    const newEvent = new Event({ ...req.body, approved: true })
-    newEvent.save()
-    res.send(newEvent)
-})
+router.put("/event/:id", async (req, res) => {
+	const { id } = req.params;
+	await Event.findOneAndDelete({ _id: id });
+	const newEvent = new Event({ ...req.body, approved: true });
+	newEvent.save();
+	res.send(newEvent);
+});
 
-router.put('/comment/:id', async (req, res) => {
-    const { id } = req.params
-    let newComment = req.body
-    console.log(req.body);
-    Event.find({ _id: id }, (err, event) => {
-        console.log(event);
-        event[0].discussion.push(newComment)
-        console.log(event);
-        event[0].save()
-        res.send(event)
-    })
-    // let relEvent = await Event.find({_id: id})
-    // relEvent[0].discussion.push(...req.body)
-    // await relEvent[0].save()
-    // res.send(relEvent[0].discussion)
-})
+router.put("/comment/:id", async (req, res) => {
+	const { id } = req.params;
+	let newComment = req.body;
+	console.log(req.body);
+	Event.find({ _id: id }, (err, event) => {
+		console.log(event);
+		event[0].discussion.push(newComment);
+		console.log(event);
+		event[0].save();
+		res.send(event);
+	});
+	// let relEvent = await Event.find({_id: id})
+	// relEvent[0].discussion.push(...req.body)
+	// await relEvent[0].save()
+	// res.send(relEvent[0].discussion)
+});
 
-router.get('/suggestions', async (req, res) => {
-    let suggestions = await Event.find({approved: false}).exec()
-    res.send(suggestions)
-})
+router.get("/suggestions", async (req, res) => {
+	let suggestions = await Event.find({ approved: false }).exec();
+	res.send(suggestions);
+});
 
-
-
-module.exports = router
+module.exports = router;
--- src/stores/EventStore.js
+++ src/stores/EventStore.js
@@ -1,35 +1,40 @@
 import { observable, computed, action, makeObservable } from "mobx";
-import axios from 'axios'
+import axios from "axios";
 export class EventsStore {
+	constructor() {
+		this.events = [];
+		this.user = null;
+		this.dateRange = [1600, 2000];
+		this.countries = [];
+		this.currentSuggestion = {};
 
-	
-    constructor() {
-        this.events = []
-        this.user = null
-        this.dateRange = [1600, 2000]
-        this.countries = [];
+		makeObservable(this, {
+			events: observable,
+			user: observable,
+			dateRange: observable,
+			setDateRange: action,
+			countries: observable,
+            currentSuggestion: observable,
+            currentSuggestionFunction:action
+		});
+	}
 
-        makeObservable(this, {
-            events: observable,
-            user: observable,
-            dateRange: observable,
-            setDateRange: action,
-            countries: observable
-        })
-    }
+	setDateRange = (val) => {
+		this.dateRange = val;
+	};
 
-    setDateRange = (val) => {
-        this.dateRange = val
-    }
-
-    userLogin = async (user) => {
-        const data = await axios.post("http://localhost:4200/login", user)
-        if(data.data){
-            this.user = data.data
-            console.log(this.user)
-        }else{
-           alert('username or password is not correct') 
-        }
-        
+	userLogin = async (user) => {
+		const data = await axios.post("http://localhost:4200/login", user);
+		if (data.data) {
+			this.user = data.data;
+			console.log(this.user);
+		} else {
+			alert("username or password is not correct");
+		}
+    };
+    
+    currentSuggestionFunction = (suggestion) => {
+        console.log(suggestion)
+        this.currentSuggestion = suggestion
     }
 }
