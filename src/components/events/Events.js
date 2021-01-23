5332ab38f3630ec93003928d83c4dc2c2612e25f
idangez95 <idangez95@gmail.com>
2021-01-24 01:55:14 +0200

    s

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -1,22 +1,11 @@
 import { Link } from 'react-router-dom';
 import { inject, observer} from 'mobx-react';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
 import React from 'react'
 
 function Events(props){
     let {events} = props.EventsStore
 
     console.log(events)
-
-    const [value, setValue] = React.useState('female');
-
-    const handleChange = (event) => {
-        setValue(event.target.value);
-    };
     
     return(
         <div>
@@ -31,16 +20,7 @@ function Events(props){
                     <img src={event.gallery[0].imageURL} alt='' />
                 </Link>
             </div>
-            )})}
-
-            <FormControl component="fieldset">
-            <FormLabel component="legend">Gender</FormLabel>
-            <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-                <FormControlLabel value="female" control={<Radio />} label="Female" />
-                <FormControlLabel value="male" control={<Radio />} label="Male" />
-            </RadioGroup>
-            </FormControl>
-  
+            )})}  
         </div>
     )
 }
