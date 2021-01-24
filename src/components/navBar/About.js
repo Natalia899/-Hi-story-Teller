814ea55c7374bb333a1643a853ec972196c282d1
toam <toamcohen.transformation@gmail.com>
2021-01-24 21:20:58 +0200

    separate-navbar

--- src/components/navBar/About.js
+++ src/components/navBar/About.js
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import React, { Component } from "react";
+import React from "react";
+
+
 const About = () => {
 	window.responsiveVoice.speak(
 		"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa ipsum quam fugiat, vero error eum enim ab, architecto recusandae nemo velit minus earum maiores inventore debitis ex, nesciunt dignissimos quidem."
