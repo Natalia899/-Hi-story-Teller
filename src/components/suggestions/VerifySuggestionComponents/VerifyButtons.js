98222582d19b6df2292a9ec4ced507e8c464899e
toam <toamcohen.transformation@gmail.com>
2021-01-26 00:25:50 +0200

    fixing bugges

--- src/components/suggestions/VerifySuggestionComponents/VerifyButtons.js
+++ src/components/suggestions/VerifySuggestionComponents/VerifyButtons.js
@@ -2,24 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "date-fns";
 import Button from "@material-ui/core/Button";
+import { inject, observer } from "mobx-react";
 
-export const VerifyButtons = (props) => {
-	return (
-		<>
-			<Link to='/suggestionsList'>
-				<Button
-					onClick={() => props.EventsStore.approveSuggestion(props.suggestion._id)}
-				>
-					APPROVE
-				</Button>
-			</Link>
-			<Link to='/suggestionsList'>
-				<Button
-					onClick={() => props.EventsStore.deleteSuggestion(props.suggestion._id)}
-				>
-					Reject
-				</Button>
-			</Link>
-		</>
-	);
-};
+export const VerifyButtons = inject("EventsStore")(
+	observer((props) => {
+		return (
+			<>
+				<Link to='/suggestionsList'>
+					<Button
+						onClick={() =>
+							props.EventsStore.approveSuggestion(props.suggestion._id)
+						}
+					>
+						APPROVE
+					</Button>
+				</Link>
+				<Link to='/suggestionsList'>
+					<Button
+						onClick={() =>
+							props.EventsStore.deleteSuggestion(props.suggestion._id)
+						}
+					>
+						Reject
+					</Button>
+				</Link>
+			</>
+		);
+	})
+);
